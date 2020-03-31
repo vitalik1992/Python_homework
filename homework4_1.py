9efe0fa67660481bac464e3ba6ec61e5854281bd
vitalik1992 <30603313+vitalik1992@users.noreply.github.com>
2020-03-31 18:43:54 +0300

    Add files via upload

---
+++ homework4_1.py
@@ -0,0 +1,14 @@
+str1 = 'Beautiful is better than ugly'
+str2 = 'Explicit is better than implicit'
+str3 = 'Simple is better than complex'
+str4 = 'Complex is better than complicated'
+str5 = 'Readability counts'
+
+print(str1.upper(),'\n', str2.upper(),'\n', str3.upper(),'\n', str4.upper(),'\n', str5.upper())
+print()
+
+print(str1.lower(),'\n', str2.lower(),'\n', str3.lower(),'\n', str4.lower(),'\n', str5.lower())
+
+
+
+
